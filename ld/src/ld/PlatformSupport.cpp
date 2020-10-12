459d488314f0b0280e66c8ddda616571f9cec54c
Michael Eisel <michael.eisel@gmail.com>
2020-10-12 08:35:37 -0400

    Fix tapi simulator warnings (#66)
    * Fix tapi simulator warnings
    
    * Fix tapi simulator warnings

--- ld/src/ld/PlatformSupport.cpp
+++ ld/src/ld/PlatformSupport.cpp
@@ -176,6 +176,21 @@ void VersionSet::checkDylibCrosslink(const VersionSet& dylibPlatforms, const std
                         if ( (getenv("RC_XBS") != NULL) && (getenv("RC_BUILDIT") == NULL) )
                             break;
                     case PlatEnforce::warning: {
+                        // zld: if the .tbd supports macOS, and cmdLinePlatform is some simulator platform, then
+                        // allow it to occur. Otherwise, it seems with tapi v4 files in Xcode 12, there are system .tbd files
+                        // in that folder without the right simulators included in their platform list
+                        bool isSimulator = cmdLinePlatform == Platform::iOS_simulator || cmdLinePlatform == Platform::tvOS_simulator || cmdLinePlatform == Platform::watchOS_simulator;
+                        auto slashPos = targetPath.rfind("/");
+                        bool isSystem = false;
+                        if (slashPos != std::string::npos) {
+                            auto basename = targetPath.substr(slashPos + 1);
+                            if (basename.find("libsystem_") == 0 || targetPath.rfind("/host/") == slashPos - 5) {
+                                isSystem = true;
+                            }
+                        }
+                        if (isSimulator && isSystem && dylibPlatforms.contains(Platform::macOS)) {
+                            break;
+                        }
                         if ( !warned ) {
                             warning("building for %s, but linking in %s file (%s) built for %s",
                                     to_str().c_str(),  dylibType.c_str(), targetPath.c_str(), dylibPlatforms.to_str().c_str());
