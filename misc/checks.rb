14dc099508a1b8d5973714db2870e16c10f03ab4
Michael Eisel <michael.eisel@gmail.com>
2021-06-25 14:49:43 -0400

    commit

--- misc/checks.rb
+++ misc/checks.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 
+raise "Building from source not currently supported for arm64, please use prebuilt instead" if `uname -m`.chomp == "arm64"
 raise "cmake not found in system path" unless system('which cmake')
